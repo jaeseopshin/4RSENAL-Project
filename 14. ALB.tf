7c9fa70c88ac44ef2d5aca8db8c6f8d77cec6d15
jaeseopshin <powerlab123@naver.com>
2026-05-25 15:49:28 +0900

    Add files via upload

--- 14. ALB.tf
+++ 14. ALB.tf
@@ -1,5 +1,4 @@
 ####### lb 생성
-
 resource "aws_lb" "web-lb" {
   name               = "web-lb"
   subnets            = [aws_subnet.terraform-pub-subnet-2a.id, aws_subnet.terraform-pub-subnet-2c.id]
@@ -9,32 +8,34 @@ resource "aws_lb" "web-lb" {
     "Name" = "web-lb"
   }
 
-  # 확인하신 보안 그룹 리소스 이름과 매칭 완료되었습니다.
+  # ALB 전용 보안 그룹으로 올바르게 매칭되어 있습니다.
   security_groups = [aws_security_group.terraform-sg-alb.id]
 }
 
 ############## Target Group
-
 resource "aws_lb_target_group" "terraform-prd-tg" {
   name     = "terraform-prd-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.terraform-vpc.id
+
+  # [수정] Nginx 웹 서버가 HTML 파일을 확실하게 응답하는지 체크하도록 경로를 명시했습니다.
   health_check {
     port = 80
-    path = "/"
+    path = "/index.html"
   }
+
   tags = {
     "Name" = "terraform-prd-tg"
   }
 }
 
 ############Listener 
-
 resource "aws_lb_listener" "terraform-prd-listener" {
   load_balancer_arn = aws_lb.web-lb.arn
   port              = "80"
   protocol          = "HTTP"
+
   default_action {
     target_group_arn = aws_lb_target_group.terraform-prd-tg.arn
     type             = "forward"
--- 9. EC2.tf
+++ 9. EC2.tf
@@ -1,10 +1,21 @@
+# 1. AWS로부터 최신 Amazon Linux 2023 이미지 ID를 실시간으로 조회
+data "aws_ami" "bastion_amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+# 2. Bastion EC2 생성
 resource "aws_instance" "terraform-pub-ec2-bastion-2a" {
-  # [수정] 서울 리전 전용 Amazon Linux 2023 공식 AMI ID로 고정
-  ami                         = "ami-09c647964e09aae1e"
+  ami                         = data.aws_ami.bastion_amazon_linux_2023.id # 자동 조회된 최신 ID 매칭
   instance_type               = "t3.micro"
   vpc_security_group_ids      = [aws_security_group.terraform-sg-bastion.id]
   subnet_id                   = aws_subnet.terraform-pub-subnet-2a.id
-  key_name                    = "soonge97"
+  key_name                    = aws_key_pair.soonge97_aws_key.key_name # 리소스 참조로 순서 보장
   associate_public_ip_address = true
 
   root_block_device {
